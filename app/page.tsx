0adb946b38e195c89279440ca47610120d7a983d
Leonardo Abreu <leonardo.abreu.dev@gmail.com>
2024-05-08 21:29:14 -0300

    fix: cleaning page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,11 @@
-'use client'
-
-import { useState } from 'react'
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from './components/ui/carousel'
-import { imgsCarousel } from './database'
-import Modal from './components/Modal'
+import CarouselImgs from './components/CarouselImgs'
+import Header from './components/Header'
 
 export default function Home() {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const [linkImg, setLinkImg] = useState('')
-
-  const openModal = (img: string) => {
-    setLinkImg(img)
-    setIsModalOpen(true)
-  }
-
-  const closeModal = (): void => {
-    setIsModalOpen(false)
-  }
   return (
-    <main className='bg-blue-900 h-screen'>
-      <Carousel className='max-w-3xl mx-auto pt-8 px-8 xl:px-0 flex flex-col gap-6 p-16'>
-        <CarouselContent>
-          {imgsCarousel.map((img) => {
-            return (
-              <CarouselItem
-                className='flex items-center justify-center  hover:scale-110'
-                key={img.id}
-              >
-                <button className='w-max' onClick={() => openModal(img.link)}>
-                  <img src={img.link} alt='' />
-                </button>
-              </CarouselItem>
-            )
-          })}
-        </CarouselContent>
-        <div className='w-full flex justify-center gap-8'>
-          <CarouselPrevious />
-          <CarouselNext />
-        </div>
-      </Carousel>
-      <Modal isOpen={isModalOpen}>
-        <div className='text-center w-4/6'>
-          <img src={linkImg} alt='' />
-          <button
-            onClick={closeModal}
-            className='mt-4 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded'
-          >
-            Fechar
-          </button>
-        </div>
-      </Modal>
-    </main>
+    <>
+      <Header />
+      <CarouselImgs />
+    </>
   )
 }
